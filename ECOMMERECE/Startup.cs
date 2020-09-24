bba8415e1d0b9a1fd13482d0ba5f0f0662c5db93
ChitraChouhan <chitra.chouhan@eternussolutions.com>
2020-09-24 18:07:55 +0530

    Add project files.

---
+++ ECOMMERECE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECOMMERECE.Startup))]
+namespace ECOMMERECE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
